6660d8217f6dd6d8008844caa9934a67ff3ea332
Adilet <adiletturdakunov2002@gmail.com>
2021-04-10 15:19:16 +0600

    63 lesson is completed

--- src/components/Content/Users/UsersContainer.js
+++ src/components/Content/Users/UsersContainer.js
@@ -10,33 +10,24 @@ import {
 } from '../../../redux/usersReducer';
 import * as axios from 'axios';
 import Users from './Users';
+import { getUsers } from '../../../api/api';
 
 class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.toggleFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-        withCredentials: true
-      })
-      .then((response) => {
-        this.props.toggleFetching(false);
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
-      });
+    getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
+      this.props.toggleFetching(false);
+      this.props.setUsers(data.items);
+      this.props.setTotalUsersCount(data.totalCount);
+    });
   }
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPages(pageNumber);
     this.props.toggleFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-        withCredentials: true
-      })
-      .then((response) => {
-        this.props.toggleFetching(false);
-        this.props.setUsers(response.data.items);
-      });
+    getUsers(pageNumber, this.props.pageSize).then((data) => {
+      this.props.toggleFetching(false);
+      this.props.setUsers(data.items);
+    });
   };
 
   render() {
